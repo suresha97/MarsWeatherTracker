097e10be5498415d953ca6d097d6f7e79327cc05
suresha97 <suresha@tcd.ie>
2021-09-25 21:10:43 +0100

    Externalise styling for app

---
+++ src/assets/app_style.py
@@ -0,0 +1,23 @@
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "18rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# padding for the page content
+CONTENT_STYLE = {
+    "margin-left": "20rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+theme = {
+    'dark': True,
+    'detail': '#007439',
+    'primary': '#00EA64',
+    'secondary': '#6E6E6E',
+}
