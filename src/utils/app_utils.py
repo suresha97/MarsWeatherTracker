def0237c98bc3b9588e56f6b2c6193195b50dda3
suresha97 <suresha@tcd.ie>
2021-09-25 21:17:23 +0100

    Refactor app utils for additional components

--- src/utils/app_utils.py
+++ src/utils/app_utils.py
@@ -1,20 +1,27 @@
+from typing import Dict, Any, Tuple, Union
 import os
 import joblib
 import json
+from collections import OrderedDict
 
+import numpy as np
 import plotly.express as px
 import pandas as pd
 import torch
+from torch import Tensor
+from matplotlib.pyplot import Figure
 
 from aws_rds.mars_weather_data_rds_database import MarsWeatherDataRDSDatabase
 from forecasting_models.forecasting_model_data import ForecastingDataModel
 from forecasting_models.models.rnn_model import RNNModel
+from forecasting_models.models.lstm_model import LSTMModel
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
 _MODEL_SAVE_PATH = "../trained_models"
 
 
-def make_plotly_graph(parameters):
+def make_plotly_graph(parameters: Dict[str, Any]) -> Figure:
     if parameters["display_forecast"] is not None and "Display Forecast" in parameters["display_forecast"]:
         parameters["plot"]["y"].append("predictions")
         parameters["column_labels"].append(f"Forecasted {get_quantity_value_from_label(parameters['plot']['y'][0])}")
@@ -22,22 +29,33 @@ def make_plotly_graph(parameters):
     if parameters["plot_type"] == "line":
         fig = px.line(**parameters["plot"])
 
-        for i, new_name in enumerate(parameters["column_labels"]):
-            fig.data[i].name = new_name
-
     elif parameters["plot_type"] == "scatter":
+        if parameters["colormap_quantity"] is not None:
+            parameters["plot"]["color"] = parameters["colormap_quantity"]
         fig = px.scatter(**parameters["plot"])
+
+        if parameters["colormap_quantity"] is not None:
+            fig.update_layout(
+                coloraxis_colorbar={
+                    "title": get_quantity_value_from_label(parameters["colormap_quantity"])
+                }
+            )
+    fig.update_layout(
+        showlegend=False
+    )
+    # for i, new_name in enumerate(parameters["column_labels"]):
+    #     fig.data[i].name = new_name
 
     return fig
 
 
-def get_latest_mars_weather_data():
+def get_latest_mars_weather_data() -> pd.DataFrame:
     mars_weather_data_rds_database = MarsWeatherDataRDSDatabase(
-        "postgres",
-        "Unn11997*",
-        "mars-weather-data.c0wmlpzuprn2.eu-west-1.rds.amazonaws.com",
-        5432,
-        "curiosity_mars_weather_data"
+        f"{os.environ.get('USER')}",
+        f"{os.environ.get('PASSWORD')}",
+        f"{os.environ.get('DATABASE_HOST')}",
+        int(os.environ.get('DATABASE_PORT')),
+        f"{os.environ.get('DATABASE_NAME')}"
     )
 
     mars_weather_data_df = mars_weather_data_rds_database.query_data_from_table_into_dataframe(
@@ -49,19 +67,18 @@ def get_latest_mars_weather_data():
     return mars_weather_data_df
 
 
-def get_weather_forecasts(weather_data, model_type, quantity):
+def get_weather_forecasts(weather_data: pd.DataFrame, model_type: str, quantity: str) -> np.ndarray:
     features, labels = prepare_data_for_model(weather_data, quantity)
     learned_model = load_latest_trained_model(model_type, quantity)
-    #learned_model = OrderedDict({k[1:]: v for k, v in learned_model.items()})
-    rnn_forecaster = RNNModel(load_model_architecture_inputs(model_type, quantity))
-    rnn_forecaster.load_state_dict(learned_model)
-    scaled_predictions = rnn_forecaster.forward(features)
+    forecaster = _get_forecaster(model_type, quantity)
+    forecaster.load_state_dict(learned_model)
+    scaled_predictions = forecaster.forward(features)
     _, scaler_y = load_scalers(quantity)
 
     return scaler_y.inverse_transform(scaled_predictions.detach())
 
 
-def prepare_data_for_model(weather_data, quantity):
+def prepare_data_for_model(weather_data: pd.DataFrame, quantity: str) -> Tuple[Tensor, Tensor]:
     weather_data.terrestrial_date = pd.to_datetime(weather_data.terrestrial_date)
     dataset_parameters = {
         "start_date": weather_data.terrestrial_date.min(),
@@ -94,7 +111,9 @@ def prepare_data_for_model(weather_data, quantity):
     return torch.tensor(scaled_features, dtype=torch.float), torch.tensor(scaled_labels, dtype=torch.float)
 
 
-def get_scaled_features_and_labels(forecasting_data, quantity):
+def get_scaled_features_and_labels(
+        forecasting_data: Dict[str, Tuple[Tensor, Tensor]], quantity: str
+) -> Tuple[np.ndarray, np.ndarray]:
     features, labels = forecasting_data["test_data"]
     features_reshape = features.reshape((features.shape[0] * features.shape[1], features.shape[2]))
     scaler_x, scaler_y = load_scalers(quantity)
@@ -102,14 +121,14 @@ def get_scaled_features_and_labels(forecasting_data, quantity):
     return scaler_x.transform(features_reshape).reshape((features.shape[0], features.shape[1], features.shape[2])), scaler_y.transform(labels)
 
 
-def load_scalers(quantity):
+def load_scalers(quantity: str) -> Tuple[Union[MinMaxScaler, StandardScaler], Union[MinMaxScaler, StandardScaler]]:
     return (
         joblib.load(f"{_MODEL_SAVE_PATH}/{quantity}/scalers/normalisation_features.gz"),
         joblib.load(f"{_MODEL_SAVE_PATH}/{quantity}/scalers/normalisation_labels.gz")
     )
 
 
-def load_latest_trained_model(model_type, quantity):
+def load_latest_trained_model(model_type: str, quantity: str) -> OrderedDict:
     saved_models = [
         file_name for file_name in os.listdir(os.path.abspath(f"{_MODEL_SAVE_PATH}/{quantity}")) if model_type in file_name
     ]
@@ -118,7 +137,7 @@ def load_latest_trained_model(model_type, quantity):
     return torch.load(f"{_MODEL_SAVE_PATH}/{quantity}/{sorted_models[0]}")
 
 
-def load_model_architecture_inputs(model_type, quantity):
+def load_model_architecture_inputs(model_type: str, quantity: str) -> Dict[str, Any]:
     saved_models = [
         file_name for file_name in os.listdir(os.path.abspath(f"{_MODEL_SAVE_PATH}/{quantity}"))
         if model_type in file_name and "inputs" in file_name
@@ -132,13 +151,24 @@ def load_model_architecture_inputs(model_type, quantity):
     return inputs
 
 
-def get_quantity_value_from_label(quantity):
+def get_quantity_value_from_label(quantity: str) -> str:
     quantity_to_label_map = {
-        "min_temp": "Temperature",
-        "max_temp": "Temperature",
-        "min_gts_temp": "Temperature",
-        "max_gts_temp": "Temperature",
-        "pressure": "Pressure"
+        "min_temp": "Minimum Air Temperature (C)",
+        "max_temp": "Maximum Air Temperature (C)",
+        "min_gts_temp": "Minimum Ground Temperature (C)",
+        "max_gts_temp": "Maximum Ground Temperature (C)",
+        "pressure": "Atmospheric Pressure (Pa)"
     }
 
     return quantity_to_label_map[quantity]
+
+
+def _get_forecaster(model_type: str, quantity: str) -> Union[RNNModel, LSTMModel]:
+    model_type_map = {
+        "rnn": RNNModel,
+        "lstm": LSTMModel
+    }
+
+    forecaster = model_type_map[model_type]
+
+    return forecaster(load_model_architecture_inputs(model_type, quantity))
--- src/utils/s3_utils.py
+++ src/utils/s3_utils.py
@@ -1,15 +1,16 @@
+from typing import Dict, Any
 import json
 
 import boto3
 
 
-def save_json_to_s3(json_obj, bucket, key):
+def save_json_to_s3(json_obj: Dict[Any, Any], bucket: str , key: str) -> None:
     s3 = boto3.resource("s3")
     s3object = s3.Object(bucket, key)
     s3object.put(Body=(bytes(json.dumps(json_obj).encode("UTF-8"))))
 
 
-def load_json_from_s3_as_dict(bucket, key):
+def load_json_from_s3_as_dict(bucket: str, key: str) -> Dict[Any, Any]:
     s3 = boto3.resource("s3")
     file_object = s3.Object(bucket, key)
     json_obj = file_object.get()["Body"].read().decode("utf-8")
